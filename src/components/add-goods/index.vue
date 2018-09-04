23b562b606aa760ef7841a62c6d9a3ca2ed0f1a4
zhangrujie <1468196979@qq.com>
2018-09-04 14:02:40 +0800

    注释二次

--- src/components/add-goods/index.vue
+++ src/components/add-goods/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="entry_b">
         <!--商品上传-->
+        <!--在注释一次-->
         <p class="entry_ts">最多上传5张</p>
         <div class="up">
             <mt-swipe :auto="3000" class="up_sw">
